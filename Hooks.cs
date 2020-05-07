e9c1c5cc90fabd55d9b9ee8d75960d0a17c837ac
FairBair <49444689+FairBear@users.noreply.github.com>
2020-05-08 01:14:58 +0800

    Koikatsu Party compatibility patch.

--- Hooks.cs
+++ Hooks.cs
@@ -25,15 +25,18 @@ namespace KK_PovX
 
 		[HarmonyPrefix]
 		[HarmonyPatch(typeof(HSceneProc), "SetShortcutKey")]
-		public static void Prefix_HSceneProc_SetShortcutKey(List<ChaControl> ___lstFemale, ChaControl ___male, ChaControl ___male1)
+		public static void Prefix_HSceneProc_SetShortcutKey(HSceneProc __instance, List<ChaControl> ___lstFemale, ChaControl ___male)
 		{
 			Controller.chaCtrls = new List<ChaControl>();
 
 			if (___male)
 				Controller.chaCtrls.Add(___male);
 
-			if (___male1)
-				Controller.chaCtrls.Add(___male1);
+			Traverse _male1 = new Traverse(__instance).Field("male1");
+			ChaControl male1 = _male1.FieldExists() ? _male1.GetValue<ChaControl>() : null;
+
+			if (male1)
+				Controller.chaCtrls.Add(male1);
 
 			foreach (ChaControl female in ___lstFemale)
 				Controller.chaCtrls.Add(female);
